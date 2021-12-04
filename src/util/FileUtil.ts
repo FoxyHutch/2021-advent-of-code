17539b893c90da1268bac2ce77b9e12049741a4f
Kester Lotz <kester.lotz@posteo.de>
2021-12-04 11:39:00 +0100

    fix(FileUtil): Remove empty rows at the end

--- src/util/FileUtil.ts
+++ src/util/FileUtil.ts
@@ -2,16 +2,18 @@ import fs = require("fs");
 import path = require("path");
 
 export class FileUtil {
-  getEntriesFromTextFileByNameAndFolder(
-    filename: string,
-    directoryPath: string
-  ): Array<string> {
+  getEntriesFromTextFileByNameAndFolder(filename: string, directoryPath: string): Array<string> {
     const inputPath = path.resolve(directoryPath, filename);
     return fs.readFileSync(inputPath).toString().split("\n");
   }
 
   getEntriesFromInputNamedTextFile(directoryPath: string): Array<string> {
     const inputPath = path.resolve(directoryPath, "input.txt");
-    return fs.readFileSync(inputPath).toString().split("\n");
+    const data = fs.readFileSync(inputPath).toString().split("\n");
+
+    while (data[data.length - 1].length < 1) {
+      data.pop();
+    }
+    return data;
   }
 }
